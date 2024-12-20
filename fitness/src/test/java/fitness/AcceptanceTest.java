a71307f2e5ac5ab46561d9259a7d79b0476ed673
dell <dell@DESKTOP-6M0GK4J.mynet>
2024-12-20 02:23:40 +0200

    seven commit

--- fitness/src/test/java/fitness/AcceptanceTest.java
+++ fitness/src/test/java/fitness/AcceptanceTest.java
@@ -1,11 +1,13 @@
 package fitness;
+
+
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType; 
 
 @RunWith(Cucumber .class)
-@CucumberOptions(features="use_cases/CreateProgram.feature",
+@CucumberOptions(features="Features/Instructor/Delete.feature",
 plugin= {"html:target/cucumber/wikipedia.html"},
 monochrome=true,
 snippets=SnippetType.CAMELCASE,
